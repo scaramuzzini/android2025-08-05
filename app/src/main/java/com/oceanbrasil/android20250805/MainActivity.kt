b50024b28be3b6a870893e72559d3ebcfac94d12
Mario Torres <scaramuzzini@gmail.com>
2025-08-07 18:05:45 -0400

    Formulario

---
+++ app/src/main/java/com/oceanbrasil/android20250805/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.oceanbrasil.android20250805
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.oceanbrasil.android20250805.ui.theme.Android20250805Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Android20250805Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Formulario(
+                        name = "Android",
+                        modifier = Modifier
+                                        .padding(innerPadding)
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Formulario(name: String, modifier: Modifier = Modifier) {
+    var nomeDigitado by remember { mutableStateOf("") }
+    var mensagem by remember { mutableStateOf("Hello")}
+
+    Column {
+        Text(
+            text = mensagem,
+            fontSize = 48.sp,
+            color = Color.Red,
+            modifier = modifier
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        OutlinedTextField(
+            value = nomeDigitado,
+            onValueChange = { nomeDigitado = it },
+            label = {
+                Text("Digite o seu nome")
+            }
+        )
+        Spacer(modifier = Modifier.padding(10.dp))
+        BotaoAzulOcean(onClick = {
+            Log.d("MainActivity", "O nome digitado é: $nomeDigitado")
+            mensagem = "Olá $nomeDigitado"
+//            Toast.makeText(this@MainActivity.context, "Tocou no btn!", Toast.LENGTH_LONG).show()
+        })
+    }
+}
+
+@Composable
+fun BotaoAzulOcean(onClick: () -> Unit) {
+    Button(onClick = { onClick() }
+        , colors = ButtonColors(containerColor = Color.Blue
+            , contentColor = Color.White
+            , disabledContainerColor = Color.Gray
+            , disabledContentColor = Color.Yellow)
+    ) {
+        Text("Enviar")
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun FormularioPreview() {
+    Android20250805Theme {
+        Formulario("Mário")
+    }
+}
